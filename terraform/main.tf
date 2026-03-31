9cb1e7df59b3dd3b091f807a0e6a9c8e2354a9d3
kranthi1382 <kranthi.yempelli@gmail.com>
2026-04-01 00:53:08 +0530

    test opa workflow

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "demo" {
 
 resource "aws_instance" "web" {
   ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t3.micro"   # ❌ should fail
+  instance_type = "t2.large"   # ❌ should fail
 
   tags = {
   Environment = "dev"
