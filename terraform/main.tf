56dfcef4d3a2a2c924330dd5542ec1bc97f41faf
kranthi1382 <kranthi.yempelli@gmail.com>
2026-04-06 10:07:46 +0530

    fix policy

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_s3_bucket" "demo" {
   bucket = "kranthi-demo-bucket-12345"
-  acl    = "public"   # ❌ should fail
+  acl    = "private"   # ❌ should fail
 
   tags = {
   Environment = "dev"
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "demo" {
 
 resource "aws_instance" "web" {
   ami           = "ami-0c02fb55956c7d316"
-  instance_type = "t2.large"   # ❌ should fail
+  instance_type = "t3.micro"   # ❌ should fail
 
   tags = {
   Environment = "dev"
